bef367c02b44e106cc6123c2bdacfc1a1a4082af
mehfal <38032315+mehfal@users.noreply.github.com>
2020-04-13 20:46:20 -0400

    Add files via upload

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,1 +1,22 @@
+import { Component, OnInit, OnChanges, Input, Output,EventEmitter } from "@angular/core";
+
+@Component({
+    selector: 'star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css'],
+})
+export class StarComponent implements OnChanges {
+    starWidth: number;
+    @Input() rating: number;
+
+    @Output() notify: EventEmitter<string> = new EventEmitter<string>();
+    ngOnChanges(): void {
+        this.starWidth = this.rating * 100 /5 ;
+    }
+
+    onClick(): void {
+        let str = `the rating ${this.rating} was clicked`;
+        this.notify.emit(str);
+    }
+}
 
